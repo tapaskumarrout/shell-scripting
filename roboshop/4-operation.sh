a28cb82b0ef690b6e534f4d0a3b7af702f1e278d
Tapas Rout <tapasrou@us.ibm.com>
2022-03-12 11:23:54 -0800

    New File created Input and output

--- roboshop/4-operation.sh
+++ roboshop/4-operation.sh
@@ -30,3 +30,12 @@ if [ $Name == "Hari" ]
    echo -e "\e[34mYou are not human being\e[0m"
 fi
 
+# Scenario 4
+echo Scenario 4 String Comparision by reading a value by using !=
+read -p 'Enter you Name: ' Name
+if [ $Name != "Hari" ]
+ then
+    echo -e "\e[34mYou are not human being\e[0m"
+ else
+   echo -e "\e[32mYou are human being\e[0m"
+fi
