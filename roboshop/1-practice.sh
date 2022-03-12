dd8bb4f17cb2302ae1412e57c36334f39e304b13
Tapas Rout <tapasrou@us.ibm.com>
2022-03-12 08:49:18 -0800

    Trying with -e and differnet color options

--- roboshop/1-practice.sh
+++ roboshop/1-practice.sh
@@ -4,4 +4,9 @@ echo Joy Jagganath !!!
 
 echo Welcome
 
-echo -e Hare Ramaa
+echo -e "\e[31mHare Ramaa\e[0m"
+echo -e "\e[32m Joy Jou joy\e[-m"
+echo -e "\e[33m Hello how are you \e[0m"
+echo -e "\e[34m Good Morning\e[0m"
+echo -e "\e[35m Good Luck guys\e[0m"
+
