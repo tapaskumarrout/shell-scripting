5763eebd00128436ab74abf1ec2a90afe5309bd0
Tapas Rout <tapasrou@us.ibm.com>
2022-03-12 10:42:29 -0800

    New File created Input and output

--- roboshop/4-operation.sh
+++ roboshop/4-operation.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 #Operator Comparision
-
+# Scenario 1
 if [ "Xyz" = "ABC" ]
  then
   echo "Both String are equal"
  else
    echo "Both Strings are not equal"
 
+fi
+
+# Scenario 2
+read -p 'Enter you Name' Name
+if [ $Name = "ABC"]
+ then
+    echo "\e[32mYou are human being\e[0m"
+ else
+   echo "\e[34mYou are not human being\e[0m"
 fi
