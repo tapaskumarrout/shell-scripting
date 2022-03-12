e4e738f0972ac294ddbb86bf87e16b60db64b515
Tapas Rout <tapasrou@us.ibm.com>
2022-03-12 12:02:02 -0800

    Scenarion 3 comapare with -ne , -gt, -ge, lt, -le

--- roboshop/5-NumberComparision.sh
+++ roboshop/5-NumberComparision.sh
@@ -14,8 +14,8 @@ if [ $var1 -eq $var2 ]
  fi
 
 
-# Scenario 2 with -eq
-echo Scenario 2 comapare a number to string
+# Scenario 2 comapare with =~
+echo Scenario 2 comapare with =~ Check the entered number is a number
 
 read -p 'Enter you Name: ' var1
 #read -p 'Enter you Name: ' var2
@@ -28,3 +28,44 @@ if  [[ $var1 =~ $re ]]
   else
     echo -e "\e[34mThis is not a number\e[0m"
  fi
+
+ # Scenarion 3 comapare with -ne , -gt, -ge, lt, -le
+
+ echo Scenarion 3 comapare with -ne , -gt, -ge, lt, -le
+
+ read -p 'Enter you Name: ' var1
+ read -p 'Enter you Name: ' var2
+
+ if [ $var1 -ne $var2 ]
+  then
+      echo -e "\e[32m $var1 is not equal to $var2\e[0m"
+   else
+     echo -e "\e[34m$var1 is  equal to $var2\e[0m"
+  fi
+
+  if [ $var1 -gt $var2 ]
+    then
+        echo -e "\e[32m$var1 is greater than $var2 \e[0m"
+     else
+       echo -e "\e[34m$var1 is not greater than $var2\e[0m"
+    fi
+
+   if [ $var1 -ge $var2 ]
+       then
+           echo -e "\e[32m$var1 is greater than equal to $var2 \e[0m"
+        else
+          echo -e "\e[34m$var1 is not greater than equal to $var2\e[0m"
+       fi
+  if [ $var1 -lt $var2 ]
+       then
+           echo -e "\e[32m$var1 is less than  $var2 \e[0m"
+        else
+          echo -e "\e[34m$var1 is not less than $var2\e[0m"
+       fi
+
+   if [ $var1 -le $var2 ]
+              then
+                  echo -e "\e[32m$var1 is less than equal to $var2 \e[0m"
+               else
+                 echo -e "\e[34m$var1 is not less than equal to $var2\e[0m"
+        fi
