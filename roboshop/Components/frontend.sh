5dbbb666a160dcbe02c6353d9735b062331d9de2
Tapas Rout <tapasrou@us.ibm.com>
2022-03-14 22:52:30 -0700

    common.sh created and added logfile

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -2,19 +2,19 @@
 
 source Components/common.sh
 
-echo -e "\e[32m*START Install Nginx.   \e[0m"
+print "START Install Nginx."
 yum install nginx -y
 statuschk $? "Install Nginx Webserver" &>>$LOG_FILE
 
-
+print "Enable nginx."
 systemctl enable nginx
 statuschk $? "Enable Nginx Webserver" &>>$LOG_FILE
 
-
+print "Start nginx."
 systemctl start nginx
 statuschk $? "Start Nginx Webserver" &>>$LOG_FILE
 
-
+print "Downloading frontend application"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 statuschk $? "Downloading frontend application" &>>$LOG_FILE
 
@@ -28,6 +28,6 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 statuschk $? "roboshop.conf Updation" &>>$LOG_FILE
 
-
+print "Restart nginx"
 systemctl restart nginx
 statuschk $? "Nginx Webserver Start" &>>$LOG_FILE
