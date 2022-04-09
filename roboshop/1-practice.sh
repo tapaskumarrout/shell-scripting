c534fd77093ad80ee5e325282849e41089b164f9
Tapas Rout <tapasrou@us.ibm.com>
2022-04-09 14:30:56 -0700

    Update Seed-jobs

--- roboshop/1-practice.sh
+++ roboshop/1-practice.sh
@@ -4,7 +4,7 @@ echo Joy Jagganath !!!
 
 echo Welcome
 
-echo -e "\e[31mHare Ramaa\e[0m"
+echo -e "\e[31mHare Rama \e[0m"
 echo -e "\e[32m Joy Jou joy\e[0m"
 echo -e "\e[33m Hello how are you \e[0m"
 echo -e "\e[34m Good Morning\e[0m"
